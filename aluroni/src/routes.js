7111b3f7ab202df142577545607bd87cca313b28
Karen Novaes <karen-nd@outlook.com>
2022-08-09 15:06:41 -0300

    Página Sobre

--- aluroni/src/routes.js
+++ aluroni/src/routes.js
@@ -1,4 +1,5 @@
 import Cardapio from 'pages/Cardapio';
+import Sobre from 'pages/Sobre';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from 'components/Menu';
@@ -15,6 +16,7 @@ export default function AppRouter() {
           <Route path='/' element={<PaginaPadrao />}>
             <Route index element={<Inicio />} />
             <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
           </Route>
         </Routes>
       </Router>

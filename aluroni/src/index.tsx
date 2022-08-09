dc72ba5d8bcbada4ded9a0f15d062b8df07c7b90
Karen Novaes <karen-nd@outlook.com>
2022-08-09 09:56:58 -0300

    add Pratos Recomendados

--- aluroni/src/index.tsx
+++ aluroni/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "normalize.css";
+import 'normalize.css';
 import './index.css';
 import Cardapio from './pages/Cardapio';
+import Inicio from 'pages/Inicio';
 
 
 const root = ReactDOM.createRoot(
@@ -10,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Cardapio />
+    <Inicio />
   </React.StrictMode>
 );
 
--- aluroni/src/pages/Cardapio/Itens/index.tsx
+++ aluroni/src/pages/Cardapio/Itens/index.tsx
@@ -1,4 +1,4 @@
-import cardapio from './itens.json'; 
+import cardapio from 'data/cardapio.json'; 
 import Item from './Item';
 import styles from './Itens.module.scss';
 import { useEffect, useState } from 'react';
@@ -21,29 +21,29 @@ export default function Itens(props: Props) {
   }
   function ordenar(novaLista: typeof cardapio){
     switch(ordenador){
-      case 'porcao':
-        return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
-      case 'qtd_pessoas':
-        return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
-        case 'preco':
-          return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
-        default:
-          return novaLista;
-      }
+    case 'porcao':
+      return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+    case 'qtd_pessoas':
+      return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
+    case 'preco':
+      return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
+    default:
+      return novaLista;
+    }
   }
   useEffect(() => {
     const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
     setLista(ordenar(novaLista));
-  }, [busca, filtro, ordenador])
+  }, [busca, filtro, ordenador]);
 
   return (
     <div className={styles.itens}>
       {lista.map(item => (
         <Item 
-        key={item.id}
-        {...item}
+          key={item.id}
+          {...item}
         />
       ))}
     </div>
-  )
+  );
 }

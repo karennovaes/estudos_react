6096e393febab4b13a1b4a3e768370d12b3868cb
Karen Novaes <karen-nd@outlook.com>
2022-08-09 14:20:43 -0300

    Página padrão - refatorando o código

--- aluroni/src/routes.js
+++ aluroni/src/routes.js
@@ -2,21 +2,21 @@ import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from 'components/Menu';
-import styles from './Routes.module.scss';
+import PaginaPadrao from 'components/PaginaPadrao';
+
 
 export default function AppRouter() {
   return (
     <main>
       <Router>
         <Menu />
-        <header className={styles.header}>
-          <div className={styles.header__text}>
-            A casa do código e da massa
-          </div>
-        </header>
+
         <Routes>
-          <Route path='/' element={<Inicio />} />
-          <Route path='/cardapio' element={<Cardapio />} />
+          <Route path='/' element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+          </Route>
+
         </Routes>
       </Router>
     </main>

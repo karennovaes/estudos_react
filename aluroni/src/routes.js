de547343bf7a80bdabfa66c6365d067d7321bc28
Karen Novaes <karen-nd@outlook.com>
2022-08-09 11:37:06 -0300

    Reaproveitando o Header

--- aluroni/src/routes.js
+++ aluroni/src/routes.js
@@ -1,16 +1,24 @@
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from 'components/Menu';
+import styles from './Routes.module.scss';
 
-export default function AppRouter(){
+export default function AppRouter() {
   return (
-    <Router>
-      <Menu />
-      <Routes>
-        <Route path='/' element={<Inicio />} />
-        <Route path='/cardapio' element={<Cardapio />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu />
+        <header className={styles.header}>
+          <div className={styles.header__text}>
+            A casa do código e da massa
+          </div>
+        </header>
+        <Routes>
+          <Route path='/' element={<Inicio />} />
+          <Route path='/cardapio' element={<Cardapio />} />
+        </Routes>
+      </Router>
+    </main>
   );
 }
